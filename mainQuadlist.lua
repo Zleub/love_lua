0865fd77df3c861c17c2c3e3b7ebbf6c3001095b
Arnaud Debray <debray.arnaud@gmail.com>
2014-09-24 18:27:13 +0200

    tmp

---
+++ mainQuadlist.lua
@@ -0,0 +1,60 @@
+function data_print(data)
+	love.graphics.print(data, 0, i)
+	i = i + 11
+end
+
+function load_tilesets(data)
+	table.insert(images, love.graphics.newImage(data.image))
+end
+
+function print_images(data)
+	love.graphics.draw(data)
+end
+
+function love.load()
+	love.graphics.print("loading")
+	pretty = require 'pl.pretty'
+	tablex = require 'pl.tablex'
+
+	map = require 'pokemap'
+	tilesets = map.tilesets
+
+	layers = map.layers
+
+	-- images = {}
+	-- tablex.foreach(map.tilesets, load_tilesets)
+	Quadlist_mod = require 'Quadlist'
+	Quadtileset = Quadlist:new(tilesets[1])
+
+end
+
+function draw_map(layer)
+	local i
+	local j
+	local k
+
+	local m = 0
+	local n = 0
+
+	k = 1
+	j = 0
+	while j < layer.height do
+		i = 1
+		m = 0
+		while i <= layer.width do
+			love.graphics.draw(Quadtileset[0], Quadtileset[layer.data[k] ], m, n)
+			m = m + 15
+			i = i + 1
+			k = k + 1
+		end
+		n = n + 15
+		j = j + 1
+	end
+end
+
+function love.draw()
+	-- tablex.foreach(images, print_images)
+
+	-- love.graphics.print( pretty.write(images, '  ', 1) )
+	draw_map(layers[1])
+end
