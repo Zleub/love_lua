804cefc346912bc82f683e5f46af92a6fac7e0bf
Arnaud Debray <debray.arnaud@gmail.com>
2014-10-14 08:25:02 +0200

    save home

--- main.lua
+++ main.lua
@@ -1,26 +1,27 @@
+pretty = require 'pl.pretty'
+
 function love.load()
-   lg = love.graphics
-   li = love.image
-   lf = love.filesystem
-   w,h = lg.getWidth(),lg.getHeight()
+	heros = {
+		love.graphics.newImage("images/Hero3.png"),
+		love.graphics.newImage("images/Hero1.png"),
+		love.graphics.newImage("images/Hero2.png"),
+		love.graphics.newImage("images/Hero3.png"),
+		love.graphics.newImage("images/Hero1.png")
+	}
 
-   transitions = lf.getDirectoryItems('transitions')
+	index = 3
+   transition_left = love.graphics.newImage('transitions/Untitled_left.png')
+ transition_right = love.graphics.newImage('transitions/Untitled_right.png')
 
-   transIndex = 1
-   transition = lg.newImage('transitions/' .. transitions[transIndex])
-
-   image = lg.newImage('test.jpg')
-
-   effect = lg.newShader [[
+	effect = love.graphics.newShader [[
       extern Image trans;
-      extern number time;
-      extern number duration;
+      extern number max_w;
       vec4 effect(vec4 color,Image tex,vec2 tc,vec2 pc)
       {
          vec4 img_color = Texel(tex,tc);
          vec4 trans_color = Texel(trans,tc);
-         number white_level   = (trans_color.r + trans_color.b + trans_color.b)/3;
-         number max_white   = time/duration;
+         number white_level   = (trans_color.r + trans_color.g + trans_color.b)/3;
+         number max_white   = max_w;
 
          if (white_level <= max_white)
          {
@@ -32,44 +33,76 @@ function love.load()
       }
    ]]
 
-   t = 0
-   duration = 1
-   effect:send("duration",duration)
-   effect:send("trans",transition)
+   effect:send("trans", transition_left)
+   effect:send("max_w", 1)
 
-   canvas = lg.newCanvas(lg.getWidth(),lg.getHeight())
-   canvas:renderTo(function() lg.draw(image) end)
+   x = heros[index]:getDimensions()
+   y = 0
+   t = 0
+
+   rate = 3
 end
+
 function love.update(dt)
-   t = t + dt
-   effect:send("time",t)
+	t = t + dt
+	h_width, h_height = heros[index]:getDimensions()
+	w_width, w_height = love.window.getDimensions()
+
+
+	if x < h_width then
+		x = x + dt * 100 * rate
+		if x > h_width then
+			x = h_width
+		end
+	elseif x > h_width then
+		x = x - dt * 100 * rate
+		if x < h_width then
+			x = h_width
+		end
+	else
+		animate = 0
+	end
+
+	effect:send("max_w", t * rate)
 end
 
 function love.keypressed(key)
-   if key == 'r' then
-      t = 0
-   end
+	if key == 'r' then
+		x = heros[index]:getDimensions()
+		y = 0
+		t = 0
+	end
 
-   if key == 'left' or key == 'right' then
-      if key == 'left' then
-         if transIndex >=2 then
-            transIndex = transIndex - 1
-         end
-      elseif key == 'right' then
-         if transIndex <= #transitions-1 then
-            transIndex = transIndex + 1
-         end
-      end
-      transition = lg.newImage('transitions/' .. transitions[transIndex])
-      effect:send("trans",transition)
-      t = 0
-   end
+	if key == 'right' then
+		index = index - 1
+		if index < 2 then
+			index = 4
+		end
+		t = 0
+		x = h_width  * 2
+		y = 0
+		animate = 1
+		effect:send("trans", transition_right)
+	elseif key == 'left' then
+		index = index + 1
+		if index > 4 then
+			index = 2
+		end
+		t = 0
+		x = 0
+		y = 0
+		animate = 1
+		effect:send("trans", transition_left)
+		end
+
 end
 
 function love.draw()
-   lg.setShader(effect)
-   lg.draw(image)
-   lg.setShader()
-   lg.print(love.timer.getFPS(),lg.getWidth()-30,10)
-   lg.print(transitions[transIndex],10,10)
+	if animate == 1 then
+		love.graphics.setShader(effect)
+		love.graphics.draw(heros[index], x, y)
+		love.graphics.setShader()
+	else
+		love.graphics.draw(heros[index], x, y)
+	end
 end
